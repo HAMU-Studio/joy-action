bbd695357fd4fe5c0078d93c2cd5a5096d6447b4
Hibiki0130 <g2227075@st.t-kougei.ac.jp>
2024-04-17 14:12:14 +0900

    敵つくった

---
+++ Unity Project/Assets/Scripts/Enemy/SpawnScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject Enemy;
+    public Transform SpawnPoint;
+
+    private float time;
+    private float spawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTime = 3.0f;
+        //Enemy.transform.position = SpawnPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (spawnTime < time)
+        {
+            GameObject newEnemy = Instantiate(Enemy);
+            newEnemy.transform.position = SpawnPoint.position;
+
+            time = 0;
+        }
+    }
+}
